c6eb461e80e56b5ed9215140b6142a795340b810
Rajeev Singh <rajeev@mahaswami.com>
2018-06-08 16:53:50 +0530

    change route from /users/sign_in to /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
